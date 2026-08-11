1d708409cbb9f824da609225a2409e6e05b32d75
megggggggl <megganmejia4@gmail.com>
2026-08-10 21:03:10 -0600

    SIN DOBLE SERVICE

--- src/app/features/prestador/prestador-routing.module.ts
+++ src/app/features/prestador/prestador-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainLayout } from '../../core/layout/main-layout/main-layout';
 import { Gestiones } from './pages/gestiones/gestiones';
 import { prestadorGuard } from '../../core/guards/rol.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainLayout,
+   
     canActivate: [prestadorGuard],
     children: [
       {
