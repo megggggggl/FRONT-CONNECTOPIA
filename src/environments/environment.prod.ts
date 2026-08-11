d92b2f3b6e4a851725d8f9f8d85e18e3f300942c
megggggggl <megganmejia4@gmail.com>
2026-08-10 19:48:19 -0600

    Build exitoso y configuración para Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: '/api'
+  production: true,
+  apiUrl: ' https://rethink-relight-prong.ngrok-free.dev' // 👈 URL real de tu backend en producción
 };
