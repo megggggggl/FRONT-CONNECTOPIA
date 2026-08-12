0b055172bff2ea889a0044e241435d75f45b70f8
Nathalia Pérez <12345678+xNaty15@users.noreply.github.com>
2026-08-12 02:24:00 +0200

    fix: permitir compilacion de produccion

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,42 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'explorar',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'comunidad',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'servicios',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'bus-routes',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'lugares-turisticos',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'mapa',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'eventos',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'mis-publicaciones',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'denuncias/comunitarias',
+    renderMode: RenderMode.Client
+  },
+  {
     path: 'perfil/**',
     renderMode: RenderMode.Client
   },
